572d55588c3b6b9d7732114d38a4e08469ae63f4
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2023-07-19 19:54:40 +0300

    Update ohmyzsh install command

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,7 +31,7 @@ if [[ "$CURRENTSHELL" != "$BREWSHELL" ]]; then
 fi
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
