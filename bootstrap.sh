bc1fbb13d3a62e4f6f443644c887422c35f2be97
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2023-08-28 09:47:21 +0300

    Move symlink action to a separate script

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,25 +39,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# symlink dotfiles
-echo "symlinking dotfiles..."
-ln -sF $(pwd)/configs/zshrc ~/.zshrc
-ln -sF $(pwd)/configs/alacritty.yml ~/.alacritty.yml
-ln -sF $(pwd)/configs/tmux.conf ~/.tmux.conf
-
-ln -sF $(pwd)/configs/gitattributes ~/.gitattributes
-ln -sF $(pwd)/configs/gitconfig ~/.gitconfig
-ln -sF $(pwd)/configs/gitignore-global ~/.gitignore-global
-
-ln -sF $(pwd)/configs/finicky.js ~/.finicky.js
-ln -sF $(pwd)/configs/gemrc ~/.gemrc
-ln -sF $(pwd)/configs/lldbinit ~/.lldbinit
-
-mkdir ~/.vim
-ln -sF $(pwd)/configs/vimrc ~/.vim/vimrc
-
-touch ~/.hushlogin
-
 exec zsh
 
-echo "✓ done"
+echo "✓ bootstrap done"
---
+++ symlink.sh
@@ -0,0 +1,23 @@
+# symlink dotfiles
+echo "symlinking dotfiles..."
+
+ln -sF $(pwd)/configs/zshrc ~/.zshrc
+ln -sF $(pwd)/configs/alacritty.yml ~/.alacritty.yml
+ln -sF $(pwd)/configs/tmux.conf ~/.tmux.conf
+
+ln -sF $(pwd)/configs/gitattributes ~/.gitattributes
+ln -sF $(pwd)/configs/gitconfig ~/.gitconfig
+ln -sF $(pwd)/configs/gitignore-global ~/.gitignore-global
+
+ln -sF $(pwd)/configs/finicky.js ~/.finicky.js
+ln -sF $(pwd)/configs/gemrc ~/.gemrc
+ln -sF $(pwd)/configs/lldbinit ~/.lldbinit
+
+mkdir ~/.vim
+ln -sF $(pwd)/configs/vimrc ~/.vim/vimrc
+
+touch ~/.hushlogin
+
+exec zsh
+
+echo "✓ symlink done"
