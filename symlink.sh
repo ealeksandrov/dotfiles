983f657b8737baa4e0d43aab2a46aa3db03f1a13
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2024-01-01 21:48:35 +0300

    Migrate Alacritty yml to toml

--- symlink.sh
+++ symlink.sh
@@ -2,7 +2,7 @@
 echo "symlinking dotfiles..."
 
 ln -sF $(pwd)/configs/zshrc ~/.zshrc
-ln -sF $(pwd)/configs/alacritty.yml ~/.alacritty.yml
+ln -sF $(pwd)/configs/alacritty.toml ~/.alacritty.toml
 ln -sF $(pwd)/configs/tmux.conf ~/.tmux.conf
 
 ln -sF $(pwd)/configs/gitattributes ~/.gitattributes
